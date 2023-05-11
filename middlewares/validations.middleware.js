03814ea3bde5f1355f1b035ae357d777405efd29
cris-dangithub <cristiandaniel8080@gmail.com>
2023-05-10 22:40:10 -0500

    S6-CL2--refactor checkers pt2

--- middlewares/validations.middleware.js
+++ middlewares/validations.middleware.js
@@ -2,110 +2,137 @@ const { check } = require('express-validator');
 const { generateCheckers } = require('../utils/generateCheckers');
 // ----------- auth validations -----------
 exports.createUserValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['username', 'password', 'role'],
-    },
-    {
-      validType: 'email',
-      fieldNames: ['email'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['username', 'password', 'role'],
+      },
+      {
+        validType: 'email',
+        fieldNames: ['email'],
+      },
+    ],
+  }),
 ];
 
 exports.loginValidation = [
-  ...generateCheckers([
-    {
-      validType: 'email',
-      fieldNames: ['email'],
-    },
-    {
-      validType: 'string',
-      fieldNames: ['password'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'email',
+        fieldNames: ['email'],
+      },
+      {
+        validType: 'string',
+        fieldNames: ['password'],
+      },
+    ],
+  }),
 ];
 
 // ----------- cart validations -----------
 exports.addProductCartValidation = [
-  ...generateCheckers([
-    {
-      validType: 'numeric',
-      fieldNames: ['productId', 'quantity'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'numeric',
+        fieldNames: ['productId', 'quantity'],
+      },
+    ],
+  }),
 ];
 
 exports.updateCartValidation = [
-  ...generateCheckers([
-    {
-      validType: 'numeric',
-      fieldNames: ['productId', 'newQuantity'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'numeric',
+        fieldNames: ['productId', 'newQuantity'],
+      },
+    ],
+  }),
 ];
 
 // ----------- category validations -----------
 exports.createCategoryValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['name'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['name'],
+      },
+    ],
+  }),
 ];
 
 // ----------- product validations -----------
 exports.createProductValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['title', 'description'],
-    },
-    {
-      validType: 'numeric',
-      fieldNames: ['quantity', 'price', 'categoryId', 'userId'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['title', 'description'],
+      },
+      {
+        validType: 'numeric',
+        fieldNames: ['quantity', 'price', 'categoryId', 'userId'],
+      },
+    ],
+  }),
 ];
 
 exports.updateProductValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['title', 'description'],
-      optionals: ['title', 'description'],
-    },
-    {
-      validType: 'numeric',
-      fieldNames: ['quantity', 'price', 'categoryId', 'userId'],
-      optionals: ['quantity', 'price', 'categoryId', 'userId'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['title', 'description'],
+        optionals: ['title', 'description'],
+      },
+      {
+        validType: 'numeric',
+        fieldNames: ['quantity', 'price', 'categoryId', 'userId'],
+        optionals: ['quantity', 'price', 'categoryId', 'userId'],
+      },
+    ],
+    optionals: [
+      'title',
+      'description',
+      'quantity',
+      'price',
+      'categoryId',
+      'userId',
+    ],
+  }),
 ];
 
 // ----------- user validations -----------
 exports.updatePasswordValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['currentPassword', 'newPassword'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['currentPassword', 'newPassword'],
+      },
+    ],
+  }),
 ];
 
 exports.updateUserValidation = [
-  ...generateCheckers([
-    {
-      validType: 'string',
-      fieldNames: ['username', 'password'],
-      optionals: ['username', 'password'],
-    },
-    {
-      validType: 'email',
-      fieldNames: ['email'],
-      optionals: ['email'],
-    },
-  ]),
+  ...generateCheckers({
+    fields: [
+      {
+        validType: 'string',
+        fieldNames: ['username', 'password'],
+        optionals: ['username', 'password'],
+      },
+      {
+        validType: 'email',
+        fieldNames: ['email'],
+        optionals: ['email'],
+      },
+    ],
+    optionals: ['username', 'password', 'email'],
+  }),
 ];
--- utils/generateCheckers.js
+++ utils/generateCheckers.js
@@ -10,41 +10,39 @@ function addRequiredField(arr, field) {
   arr.push(check(`${field}`, message).not().isEmpty());
 }
 
-function genValidator(checkers, validator, message, cbString) {
+function genValidator(checkers, validator, message, cbString, validators) {
   validator.fieldNames.forEach(fieldName => {
-    if (!validator.optionals?.includes(fieldName))
+    if (!validators.optionals?.includes(fieldName))
       addRequiredField(checkers, fieldName);
     const mess = `The ${fieldName} ${message}`;
     checkers.push(check(fieldName, mess)[`${cbString}`]());
-    if (validator.optionals?.includes(fieldName)) addOptionalField(checkers);
+    if (validators.optionals?.includes(fieldName)) addOptionalField(checkers);
   });
 }
 
 /**
- *@param {[{validType: 'email' | 'numeric' |'string'; fieldNames:string[]; optionals?: string[] }]} validators
+ *@param { {fields: [{validType: 'email' | 'numeric' |'string'; fieldNames:string[] }],optionals?: string[]} } validators
  *
  */
 // Genera checkers a partir de los tipos numeric, email y string
 exports.generateCheckers = validators => {
   const checkers = [];
-  validators.forEach(validator => {
+  validators.fields.forEach(validator => {
     const { validType } = validator;
     let message;
 
     if (validType === 'email') {
       message = `must be a valid email address`;
-      genValidator(checkers, validator, message, 'isEmail');
+      genValidator(checkers, validator, message, 'isEmail', validators);
     }
     if (validType === 'numeric') {
       message = `must be a number`;
-      genValidator(checkers, validator, message, 'isNumeric');
+      genValidator(checkers, validator, message, 'isNumeric', validators);
     }
     if (validType === 'string') {
       message = `must be a string`;
-      genValidator(checkers, validator, message, 'isString');
+      genValidator(checkers, validator, message, 'isString', validators);
     }
   });
   return checkers;
 };
-
-//TODO 2:08:00
